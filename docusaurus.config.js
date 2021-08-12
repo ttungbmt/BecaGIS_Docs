b754701c82322177e638a34b5f49bd2cf64e32de
Truong Thanh Tung <ttungbmt@gmail.com>
2021-08-12 10:14:13 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://ttungbmt.com.github.io',
+  baseUrl: '/BecaGIS_Docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ttungbmt', // Usually your GitHub org/user name.
+  projectName: 'BecaGIS_Docs', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
